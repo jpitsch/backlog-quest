a82701dac955f0673560a6ec8fa520458425fb00
Jason Pitsch <pitsch.jason@gmail.com>
2023-10-17 12:09:54 -0500

    Fixed unit tests that were broken but will need to add meaningful tests later.

--- src/games/games.controller.spec.ts
+++ src/games/games.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GamesController } from './games.controller';
+import { GamesService } from './games.service';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('GamesController', () => {
   let controller: GamesController;
@@ -7,8 +11,19 @@ describe('GamesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GamesController],
-    }).compile();
-
+    }).useMocker((token) => {
+      const results = ['game1', 'game2'];
+      if (token === GamesService) {
+        return { findAll: jest.fn().mockResolvedValue(results) };
+      }
+      if (typeof token === 'function') {
+        const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
+        const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+        return new Mock();
+      }
+    })
+    .compile();
+    
     controller = module.get<GamesController>(GamesController);
   });
 
--- src/games/games.service.spec.ts
+++ src/games/games.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
 import { GamesService } from './games.service';
+import { Game } from "./schemas/game.schema";
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('GamesService', () => {
   let service: GamesService;
 
   beforeEach(async () => {
+    function mockGameModel(dto: any) {
+      this.data = dto;
+      this.create = () => {};
+      this.find = () => {};
+      this.findOneAndUpdate = () => {};
+    }
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GamesService],
+      providers: [
+        GamesService,
+        {
+          provide: getModelToken('Game'),
+          useValue: mockGameModel,
+        },
+      ],
     }).compile();
 
     service = module.get<GamesService>(GamesService);
